76b03165242db14e896419d6978b57b9601fcbb4
jascha <jascha@jaeh.at>
2020-02-17 06:06:02 +0100

    add THEME_VARS, use project and grundstein themes

--- config.mjs
+++ config.mjs
@@ -5,7 +5,7 @@ export default {
     BRANCH: 'master',
   },
   PUBLIC: 'docs',
-  THEME: '@grundstein/magic-theme',
+  THEME: ['project', 'grundstein'],
 
   WEB_ROOT: '/',
   URL: 'grundstein.github.io',
@@ -13,4 +13,40 @@ export default {
   HOIST: ['LightSwitch', 'Gdpr'],
 
   IGNORED_STATIC: ['.xcf'],
+
+  THEME_VARS: {
+    background: {
+      dark: '#212121',
+      light: '#e0e0e0',
+    },
+
+    link: {
+      dark: '#c4c4c4',
+      light: '#212121',
+
+      hover: {
+        dark: '#f09d09',
+        light: '#f09d09',
+      },
+    },
+
+    neutral: '#5a5a5a',
+
+    primary: {
+      neutral: '#f09d09',
+      light: '#f5ff5f',
+      dark: '#e05c05',
+    },
+
+    secondary: {
+      neutral: '#5a5a5a',
+      light: '#d8d8d8',
+      dark: '#0a0a0a',
+    },
+
+    text: {
+      dark: '#c4c4c4',
+      light: '#212121',
+    },
+  },
 }
