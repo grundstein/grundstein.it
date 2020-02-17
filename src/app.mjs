94115f66124d3cf41354a2d3f5ef8d2fb69e9e62
jascha <jascha@jaeh.at>
2020-02-17 06:04:51 +0100

    add theme vars for project theme

--- src/app.mjs
+++ src/app.mjs
@@ -1,6 +1,9 @@
 export const state = config => ({
   title: '@grundstein',
+
+  branding: ['@grund', 'stein'],
   description: 'the wise build their house on a rock. hosting without hassle.',
+
   seo: {
     name: '@grundstein',
     url: `https://${config.URL}`,
@@ -14,5 +17,37 @@ export const state = config => ({
       url: 'https://jaeh.at',
     },
   },
+
   menu: [{ to: '/', text: 'about' }],
+
+  hero: {
+    title: ['@grund', 'stein'],
+    description: 'devops without hassle',
+    content: 'the wise build their house on a rock.',
+
+    menu: [
+      { to: '/#about', text: 'about' },
+      { to: '/getting-started/', text: 'getting started' },
+    ],
+  },
+
+  footer: {
+    one: {
+      title: '@grundstein',
+      before: ['hosting without hassle.', 'created by', 'Wizards & Witches @ Work'],
+      menu: [{ to: '/privacy/', text: 'privacy notice' }],
+    },
+    two: {
+      title: 'our projects',
+      menu: [
+        { to: 'https://webboot.org', text: 'webboot' },
+        { to: 'https://twitter.com/webbootorg', text: 'magic' },
+        { to: 'https://keybase.io/webboot', text: 'grundstein' },
+      ],
+    },
+    three: {
+      title: 'social links',
+      menu: [{ to: 'https://github.com/grundstein', text: 'github' }],
+    },
+  },
 })
