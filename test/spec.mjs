0704ee755b5ee31d0d754c8617b99cca594d33f5
jascha <jascha@jaeh.at>
2020-04-26 13:58:44 +0200

    test: update spec

--- test/spec.mjs
+++ test/spec.mjs
@@ -1,9 +1,8 @@
 import { is } from '@magic/test'
+
 import * as app from '../src/app.mjs'
-import * as page from '../src/pages/index.mjs'
 
 export default [
   { fn: () => app, expect: is.object, info: 'app is an object' },
-  { fn: () => app.state, expect: is.fn, info: 'app.state is a function' },
-  { fn: () => page.View, expect: is.fn, info: 'page.View is a function' },
+  { fn: () => app.state, expect: is.object, info: 'app.state is an object' },
 ]
